66514db626fb2e4d87b35923b851d0fc8c9b8559
Kevin <kevinkunzmann@gmx.net>
2019-04-08 17:47:36 +0100

    added example and fixed NSE note when checking

--- R/cran.R
+++ R/cran.R
@@ -2,10 +2,13 @@
 #'
 #' @param pkgname character, package name
 #'
+#' @usage
+#' get_cran_version("dplyr")
 #'
 #' @export
 get_cran_version <- function(pkgname) {
 
+  package <- NULL # avoid CRAN check note on NSE
   memoise_cran_db() %>%
     filter(package == pkgname) %>%
     pull(version)
