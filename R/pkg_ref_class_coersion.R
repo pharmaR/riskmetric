4bcbf917472ee489efdf8b2e011d3d555fac9fd6
dgkf <18220321+dgkf@users.noreply.github.com>
2019-08-03 11:27:47 -0700

    broke up pkg_ref class into a few more focused files

---
+++ R/pkg_ref_class_coersion.R
@@ -0,0 +1,19 @@
+#' @export
+as_tibble.pkg_ref <- function(x, ...) {
+  as_tibble(vctrs::new_list_of(list(x),
+    ptype = pkg_ref(),
+    class = "list_of_pkg_ref"))
+}
+
+
+
+#' @export
+as_tibble.list_of_pkg_ref <- function(x, ...) {
+  package_names <- vapply(x, "[[", character(1L), "name")
+  versions <- vapply(x, function(xi) as.character(xi$version), character(1L))
+
+  dplyr::tibble(
+    package = package_names,
+    version = versions,
+    pkg_ref = x)
+}
---
+++ R/pkg_ref_class_extract.R
@@ -0,0 +1,83 @@
+#' @export
+`$.pkg_ref` <- function(x, name) {
+  `[[`(x, as.character(name))
+}
+
+
+
+#' @export
+`$<-.pkg_ref` <- function(x, name, value) {
+  `[[<-`(x, name, value = value)
+}
+
+
+
+#' @export
+`[[.pkg_ref` <- function(x, name, ...) {
+  if (!name %in% names(x))
+    allow_mutation(x, pkg_ref_cache(x, name))
+  bare_env(x, x[[name, ...]])
+}
+
+
+
+#' @export
+`[[<-.pkg_ref` <- function(x, name, value) {
+  if (is.null(attr(x, "allowed_mutations")))
+    stop(pkg_ref_mutability_error(name))
+  bare_env(x, x[[name]] <- value)
+}
+
+
+
+#' @export
+`[.pkg_ref` <- function(x, names, ...) {
+  lapply(names, function(n, ...) x[[n, ...]], ...)
+}
+
+
+
+#' @export
+`[<-.pkg_ref` <- function(x, names, value) {
+  invisible(Map(function(name, value) {
+    `[[<-`(x, name = name, value = value)
+  }, names, value))
+}
+
+
+bare_env <- function(x, expr, envir = parent.frame()) {
+  old_class <- class(x)
+  class(x) <- "environment"
+  on.exit(class(x) <- old_class)
+  eval(expr, envir = envir)
+}
+
+
+pkg_ref_mutability_error <- function(name) {
+  message <- list(paste0(
+    "Assignment to a pkg_ref environment can only be done in a ",
+    "pkg_ref_cache call."))
+
+  if (!missing(name)) message <- append(message, list(paste0(
+    "Extend the pkg_ref class by implementing function '",
+    "pkg_ref_cache.", name, "'")))
+
+  e <- simpleError(message = paste(message, collapse = " "))
+  class(e) <- c("pkg_ref_mutability_error", "pkg_ref_error", class(e))
+  e
+}
+
+
+allow_mutation <- function(x, expr, envir = parent.frame()) {
+  expr <- substitute(expr)
+
+  if (is.null(attr(x, "allowed_mutations"))) attr(x, "allowed_mutations") <- 0
+  attr(x, "allowed_mutations") <- attr(x, "allowed_mutations") + 1
+
+  on.exit({
+    attr(x, "allowed_mutations") <- attr(x, "allowed_mutations") - 1
+    if (attr(x, "allowed_mutations") <= 0) attr(x, "allowed_mutations") <- NULL
+  })
+
+  eval(expr, envir = envir)
+}
