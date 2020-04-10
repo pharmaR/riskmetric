b231399d8ae987de9fb9c37fb7dbaf8e496203fd
Yilong Zhang <zhanyilo@ctchpcpe001.merck.com>
2020-04-09 23:40:21 -0400

    extract release date

---
+++ R/pkg_ref_cache_release_date.R
@@ -0,0 +1,53 @@
+#' Cache a List of Package Release Date from a Package Reference
+#'
+#' @inheritParams pkg_ref_cache
+#' @family package reference cache
+#'
+pkg_ref_cache.release_date <- function(x, name, ...) {
+  UseMethod("pkg_ref_cache.release_date")
+}
+
+
+pkg_ref_cache.release_date.pkg_remote <- function(x, name, ...) {
+
+  db  <- rvest::html_table(x$web_html)[[1]]
+  date <- db[grep("Publish",db[,1], ignore.case = TRUE) ,2]
+  date
+}
+
+
+pkg_ref_cache.release_date.pkg_install <- function(x, name, ...) {
+  x$description[, "Date"]
+}
+
+
+
+pkg_ref_cache.release_date.pkg_source <- function(x, name, ...) {
+  x$description[, "Date"]
+}
+
+
+#' Cache a List of Archived Package Release Date from a Package Reference
+#'
+#' @inheritParams pkg_ref_cache
+#' @family package reference cache
+#'
+pkg_ref_cache.archive_release_date <- function(x, name, ...) {
+  UseMethod("pkg_ref_cache.archive_release_date")
+}
+
+pkg_ref_cache.archive_release_date.pkg_remote <- function(x, name, ...) {
+
+  url <- sprintf("%s/src/contrib/Archive/%s", x$repo_base_url, x$name)
+
+  html <- xml2::read_html(url)
+  node <- rvest::html_node(html, "pre")
+
+  text <- unlist(strsplit(rvest::html_text(node), "\n"))
+  db   <- do.call(rbind, strsplit(text[-1], "\\s+"))
+  version <- stringr::str_extract(db[,2], "[0-9]*.[0-9]*-[0-9]*")
+  date  <- db[,3]
+  cbind(name = x$name, version, date)
+
+}
+
