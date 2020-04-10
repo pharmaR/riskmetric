fee22a02ef5dea418ef2137c42bf520ee9aac40b
Yilong Zhang <zhanyilo@ctchpcpe001.merck.com>
2020-04-09 23:39:58 -0400

    update description to upper cases.
    Lower case is not working in a Linux system.

--- R/pkg_ref_cache_description.R
+++ R/pkg_ref_cache_description.R
@@ -10,11 +10,11 @@ pkg_ref_cache.description <- function(x, name, ...) {
 
 
 pkg_ref_cache.description.pkg_install <- function(x, name, ...) {
-  read.dcf(file.path(x$path, "description"))
+  read.dcf(file.path(x$path, "DESCRIPTION"))
 }
 
 
 
 pkg_ref_cache.description.pkg_source <- function(x, name, ...) {
-  read.dcf(file.path(x$path, "description"))
+  read.dcf(file.path(x$path, "DESCRIPTION"))
 }
