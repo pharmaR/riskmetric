c7ad828eac3cd382101d6b381e56cf5848f6ab4b
Eli Miller <Elimillera@gmail.com>
2020-04-19 19:26:01 -0500

    Add assessment and score for has_bug_reports_url metric

---
+++ R/assess_has_bug_reports_url.R
@@ -0,0 +1,27 @@
+#' Assess a package for the presence of a url field where bugs can be reported.
+#'
+#' @eval roxygen_assess_family("has_bug_reports_url", 
+#' "a character value containing the BugReports field contents")
+#'
+#' @export
+assess_has_bug_reports_url <- function(x, ...){
+  has_bug_reports_url <- x$bug_reports_url
+  pkg_metric(has_bug_reports_url, class = "pkg_metric_has_bug_reports_url") 
+}
+
+# assign a friendly name for assess column
+attr(assess_has_bug_reports_url,"column_name") <- "has_bug_reports_url"
+attr(assess_has_bug_reports_url,"label") <- "presence of a bug_reports_url in repository"
+
+#' Score a package for the presence of a bug report url
+#'
+#' @eval roxygen_score_family("has_bug_reports_url")
+#' 
+#' @return A logical value indicating whether the package has a BugReports field
+#'   filled in
+#' 
+#' @export
+score.pkg_metric_has_bug_reports_url <- function(x, ...) {
+  # Return a score of 1 if a bug report url is found, 0 otherwise.
+  ifelse(!is.null(x), 1, 0)
+}
