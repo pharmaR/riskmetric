e9087cf605225e1aebb416186eceada7fc23fb58
Doug Kelkhoff <18220321+dgkf@users.noreply.github.com>
2020-05-10 11:03:08 -0700

    Update R/pkg_ref_cache_source_control_url.R

--- R/pkg_ref_cache_source_control_url.R
+++ R/pkg_ref_cache_source_control_url.R
@@ -1,6 +1,6 @@
 pkg_res_cache.source_control_url <- function(url){
   url <- tolower(x$pkg_website)
-  s_url <- c("github.com", "bitbucket.com", "gitlab.com")
+  s_url <- "(github\\.com|bitbucket\\.org|gitlab\\.com)"
 
   if(! is.na(url)){
     res <- list()
