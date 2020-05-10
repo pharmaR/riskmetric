4a205918a18875f6140dbdfa06991bfcff83522c
Doug Kelkhoff <18220321+dgkf@users.noreply.github.com>
2020-05-10 11:03:15 -0700

    Update R/pkg_ref_cache_source_control_url.R

--- R/pkg_ref_cache_source_control_url.R
+++ R/pkg_ref_cache_source_control_url.R
@@ -3,11 +3,7 @@ pkg_res_cache.source_control_url <- function(url){
   s_url <- "(github\\.com|bitbucket\\.org|gitlab\\.com)"
 
   if(! is.na(url)){
-    res <- list()
-    for(i in 1:length(s_url)){
-      res[[i]] <- grep(s_url[i], unlist(strsplit(url, ",")), value = TRUE)
-    }
-    res <- unlist(res)
+    res <- grep(s_url, strsplit(url, ",")[[1]], value = TRUE)
   }
 
   res
