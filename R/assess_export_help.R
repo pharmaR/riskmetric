599de0cf65f8b7b9931dfe291a9bc560bb447d76
Yilong Zhang <zhanyilo@ctchpcpe001.merck.com>
2020-05-23 18:45:47 -0400

    remove package dependency
    * remove dependency of fansi covr remote stringr
    * comment functions related to tip and hint

--- R/assess_export_help.R
+++ R/assess_export_help.R
@@ -39,33 +39,33 @@ score.pkg_metric_export_help <- function(x, ...) {
 
 
 
-#' Provide development hints for improving exported value documentation
-#' @inheritParams dev_tips
-dev_tips.pkg_metric_export_help <- function(x, ...) {
-  x_sorted <- x[order(names(x))]
-  x_sorted <- sort(x_sorted, decreasing = TRUE)
-
-  max_nchar <- max(nchar(names(x_sorted)))
-  row_n <- (getOption("width") / (max_nchar + 2)) %/% 1
-  max_nchar <- (getOption("width") / row_n) %/% 1
-
-  text <- paste0(
-    ifelse(x_sorted,
-      dev_hint_crayon_success("\u2713"),  # check mark
-      dev_hint_crayon_failure("\u2718")), # x mark
-    names(x_sorted),
-    strrep(" ", max_nchar - nchar(names(x_sorted))))
-
-  dev_hint(
-    title = "Documenting Exported Objects",
-    text = list(
-      dev_hint_section("Not all exported objects have help documentation."),
-      dev_hint_section(paste0(
-        "Consider adding [.Rd help files](https://cran.r-project.org/doc/manuals/R-exts.html#Writing-R-documentation-files) ",
-        "or use [roxygen2](https://cran.r-project.org/web/packages/roxygen2/vignettes/roxygen2.html) ",
-        "to document your functions alongside the source code.")),
-      dev_hint_section(paste0(text,
-        ifelse(seq_along(text) %% row_n == 0, "\n", ""),
-        collapse = ""),
-        list(wrap = FALSE))))
-}
+#' #' Provide development hints for improving exported value documentation
+#' #' @inheritParams dev_tips
+#' dev_tips.pkg_metric_export_help <- function(x, ...) {
+#'   x_sorted <- x[order(names(x))]
+#'   x_sorted <- sort(x_sorted, decreasing = TRUE)
+#'
+#'   max_nchar <- max(nchar(names(x_sorted)))
+#'   row_n <- (getOption("width") / (max_nchar + 2)) %/% 1
+#'   max_nchar <- (getOption("width") / row_n) %/% 1
+#'
+#'   text <- paste0(
+#'     ifelse(x_sorted,
+#'       dev_hint_crayon_success("\u2713"),  # check mark
+#'       dev_hint_crayon_failure("\u2718")), # x mark
+#'     names(x_sorted),
+#'     strrep(" ", max_nchar - nchar(names(x_sorted))))
+#'
+#'   dev_hint(
+#'     title = "Documenting Exported Objects",
+#'     text = list(
+#'       dev_hint_section("Not all exported objects have help documentation."),
+#'       dev_hint_section(paste0(
+#'         "Consider adding [.Rd help files](https://cran.r-project.org/doc/manuals/R-exts.html#Writing-R-documentation-files) ",
+#'         "or use [roxygen2](https://cran.r-project.org/web/packages/roxygen2/vignettes/roxygen2.html) ",
+#'         "to document your functions alongside the source code.")),
+#'       dev_hint_section(paste0(text,
+#'         ifelse(seq_along(text) %% row_n == 0, "\n", ""),
+#'         collapse = ""),
+#'         list(wrap = FALSE))))
+#' }
