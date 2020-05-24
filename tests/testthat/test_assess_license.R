a881c8a9da5b24272341ae6084116dbed93e8915
Eli Miller <Elimillera@gmail.com>
2020-05-24 12:38:26 -0500

    Adds two embeded packages and updates unit tests for assessments for them.
    I have created two fake packages that can mimic packages being tested from source. Next on the list is to create tests for pkg_ref_class and figureout http mocking.

---
+++ tests/testthat/test_assess_license.R
@@ -0,0 +1,10 @@
+test_source_1 <- pkg_ref("test_package_1")
+assess_source_1 <- assess(test_source_1)
+test_source_2 <- pkg_ref("test_package_2")
+assess_source_2 <- assess(test_source_2)
+
+test_that("assess_license returns the expected result for soruced packages", {
+
+  expect_equal(unclass(assess_source_1$license[[1]]), "MIT + file LICENSE")
+  expect_equal(unclass(assess_source_2$license[[1]]), "CC BY 2.0")
+})
