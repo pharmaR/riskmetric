dc2f8fff3822e104f4750f4cd661dca6898999a5
emillima <eric.milliman@biogen.com>
2020-07-21 17:33:56 -0400

    added R CMD check for source pkg_ref and accompanying assessment functions

---
+++ R/assess_cran_checks.R
@@ -0,0 +1,17 @@
+
+assess_cran_checks <- function(x, ...) {
+  UseMethod("assess_cran_checks")
+}
+attr(assess_cran_checks, "column_name") <- "cran_checks"
+
+assess_cran_checks.default <- function(x, ...) {
+  pkg_metric(warning("Package is not a CRAN reference so there are no CRAN checks to assess"), class = "pkg_metric_cran_checks")
+}
+
+assess_cran_checks.pkg_ref <- function(x, ...) {
+  pkg_metric(table(x$cran_checks[["Status"]]), class = "pkg_metric_cran_checks")
+}
+
+assess_cran_checks.pkg_source <- function(x, ...) {
+  pkg_metric(sapply(check_results[c("notes","errors","warnings")], length), class = "pkg_metric_cran_checks")
+}
--- R/pkg_ref_cache_cran_checks.R
+++ R/pkg_ref_cache_cran_checks.R
@@ -35,15 +35,24 @@ pkg_ref_cache.cran_checks.pkg_bioc_remote <- function(x, name, ...) {
   return(rst)
 }
 
-assess_cran_checks <- function(x, ...) {
-  UseMethod("assess_cran_checks")
-}
-attr(assess_cran_checks, "column_name") <- "cran_checks"
+pkg_ref_cache.cran_checks.pkg_bioc_remote <- function(x, name, ...) {
+  URLbase <- "http://bioconductor.org/checkResults/release/bioc-LATEST/"
+  webURL <- paste0(URLbase, x$name)
 
-assess_cran_checks.default <- function(x, ...) {
-  pkg_metric(warning("Package is not a CRAN reference so there are no CRAN checks assess"), class = "pkg_metric_cran_checks")
+  page <- read_html(webURL)
+  tables <- xml2::xml_find_all(page, ".//table")
+  rows <- xml_find_all(tables[[3]], "//tr")
+  rows <- rows[grepl("odd", xml_attr(rows, "class"))]
+  fields <- lapply(rows, xml_find_all, ".//td|.//th")
+  fields <- lapply(fields, function(x) x[grepl("node|status", xml_attr(x, "class"))])
+  text <- lapply(fields, xml_text, trim=T)
+  #sapply(fields, "[[", 6)[-1]
+  rst <- as.data.frame(do.call(rbind, text))
+  colnames(rst) <- sapply(xml_find_all(rows[[1]], ".//td"), xml_text, trim=TRUE)[-1]
+  return(rst)
 }
 
-assess_cran_checks.pkg_ref <- function(x, ...) {
-  pkg_metric(table(x$cran_checks[["Status"]]), class = "pkg_metric_cran_checks")
+pkg_ref_cache.pkg_source <- function(x, name, ...){
+  check_results <- devtools::check(x$path)
+  return(check_results)
 }
