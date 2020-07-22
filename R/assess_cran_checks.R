c22adb24a5237a01ff9e47e3cbf63c841051d6ab
emillima <eric.milliman@biogen.com>
2020-07-22 08:33:23 -0400

    removed pkg_ref s3 method for assess_cran_checks and replaced with methods specific for CRAN_remote and bioc_remote

--- R/assess_cran_checks.R
+++ R/assess_cran_checks.R
@@ -8,7 +8,11 @@ assess_cran_checks.default <- function(x, ...) {
   pkg_metric(warning("Package is not a CRAN reference so there are no CRAN checks to assess"), class = "pkg_metric_cran_checks")
 }
 
-assess_cran_checks.pkg_ref <- function(x, ...) {
+assess_cran_checks.pkg_cran_remote <- function(x, ...) {
+  pkg_metric(table(x$cran_checks[["Status"]]), class = "pkg_metric_cran_checks")
+}
+
+assess_cran_checks.pkg_bioc_remote <- function(x, ...) {
   pkg_metric(table(x$cran_checks[["Status"]]), class = "pkg_metric_cran_checks")
 }
 
