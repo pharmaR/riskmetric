64d03d7fd109d6568e3694cffce9b394bbf4a50a
dgkf <18220321+dgkf@users.noreply.github.com>
2020-09-28 13:53:52 -0700

    removed isFALSE usage, added info to webmockr version test name

--- tests/testthat/test_riskmetric_remotes.R
+++ tests/testthat/test_riskmetric_remotes.R
@@ -1,5 +1,13 @@
-test_that("webmockr GitHub remote dependency has been replaced by CRAN release v0.7 when available", {
-  ap <- available.packages(repos = "cloud.r-project.org")
-  CRAN_webmockr_version <- ap[ap[,"Package"] == "webmockr", "Version"]
-  expect_true(CRAN_webmockr_version < package_version("0.7"))
+
+ap <- available.packages(repos = "cloud.r-project.org")
+CRAN_webmockr_version <- ap[ap[,"Package"] == "webmockr", "Version"]
+
+test_that(
+  sprintf(
+    "webmockr remote is necessary until v0.7 release (currently v%s)",
+    CRAN_webmockr_version), {
+  expect_true({
+    length(CRAN_webmockr_version) == 0L ||
+    CRAN_webmockr_version < package_version("0.7")
+  })
 })
