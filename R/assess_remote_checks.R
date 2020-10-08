f74bd6c5d630a177b68c61638c34e5c0c564cf54
emillima <eric.milliman@biogen.com>
2020-10-08 17:05:26 -0400

    pkg_ref and assess_* functions for R checks run by remote repositories (i.e. CRAN and BioC)

---
+++ R/assess_remote_checks.R
@@ -0,0 +1,22 @@
+#' Assess package checks from CRAN/Bioc or R CMD check
+#'
+#'
+#' @export
+assess_cran_checks <- function(x, ...) {
+  UseMethod("assess_cran_checks")
+}
+attr(assess_cran_checks, "column_name") <- "cran_checks"
+
+assess_cran_checks.default <- function(x, ...) {
+  pkg_metric_na(message="Package is not a CRAN or BioC reference so there are no CRAN/BioC checks to assess")
+}
+
+#' @export
+assess_cran_checks.pkg_cran_remote <- function(x, ...) {
+  pkg_metric(table(x$cran_checks[["Status"]]), class = "pkg_metric_cran_checks")
+}
+
+#' @export
+assess_cran_checks.pkg_bioc_remote <- function(x, ...) {
+  pkg_metric(table(x$cran_checks[["Status"]]), class = "pkg_metric_cran_checks")
+}
