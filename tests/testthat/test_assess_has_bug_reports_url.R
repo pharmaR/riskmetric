299c76a577221e1d56c2a04143e4993757ed7eac
yilong zhang <elong0527@gmail.com>
2021-01-20 13:02:40 -0500

    Delete test_assess_has_bug_reports_url.R

--- tests/testthat/test_assess_has_bug_reports_url.R
+++
@@ -1,9 +0,0 @@
-test_that("assess_has_bug_reports_url returns the correct url", {
-  expect_s3_class(
-    assess_source_good$has_bug_reports_url,
-    c("pkg_metric_has_bug_reports_url", "pkg_metric", "character"))
-
-  expect_equal(
-    as.character(assess_source_good$has_bug_reports_url),
-    "https://github.com/pharmar/pkgsourcegood")
-})
