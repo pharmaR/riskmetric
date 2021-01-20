acb472ca6125321763d0da0052a00a9a6f5741df
yilong zhang <elong0527@gmail.com>
2021-01-20 13:02:18 -0500

    Delete test_assess_news_current.R

--- tests/testthat/test_assess_news_current.R
+++
@@ -1,5 +0,0 @@
-test_that("assess_news_current returns expected result for source packages", {
-  # TODO: Add other pkg_ref types
-  expect_true(assess_source_good$news_current[[1]])
-  expect_length(assess_source_bad$news_current, 0)
-})
