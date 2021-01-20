3dbcb15fb8ed4350a0bb8f9268524c093c346833
yilong zhang <elong0527@gmail.com>
2021-01-20 13:02:56 -0500

    Delete test_assess_export_help.R

--- tests/testthat/test_assess_export_help.R
+++
@@ -1,5 +0,0 @@
-# Commenting these out until this is implemented for sourced packages
-# test_that("assess_export_help returns expected result for source packages", {
-#   expect_true(assess_source_1$export_help[[1]])
-#   expect_false(assess_source_2$export_help[[1]])
-# })
