2c603f0891d850a9d9307768a1f1acf8ece6caf0
yilong zhang <elong0527@gmail.com>
2021-01-20 13:02:28 -0500

    Delete test_assess_has_news.R

--- tests/testthat/test_assess_has_news.R
+++
@@ -1,5 +0,0 @@
-test_that("assess_has_news returns expected result for source packages", {
-  # TODO: add other package types
-  expect_equal(unclass(assess_source_good$has_news[[1]]), 1)
-  expect_equal(unclass(assess_source_bad$has_news[[1]]), 0)
-})
