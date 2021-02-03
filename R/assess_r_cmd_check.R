c1b1559e951855479e42b076141eae983a942d79
emillima <eric.milliman@biogen.com>
2021-02-03 15:20:42 -0500

    change pkg_metri_na to as_pkg_metric_na, same for pkg_metric_todo

--- R/assess_r_cmd_check.R
+++ R/assess_r_cmd_check.R
@@ -15,7 +15,7 @@ attributes(assess_r_cmd_check)$label <- "Package check results"
 
 #' @export
 assess_r_cmd_check.default <- function(x, ...) {
-  pkg_metric_na(message = "Source code not available to run R CMD check on.")
+  as_pkg_metric_na(message = "Source code not available to run R CMD check on.")
 }
 
 #' @export
@@ -26,12 +26,12 @@ assess_r_cmd_check.pkg_source <- function(x, ...) {
 
 #' @export
 assess_r_cmd_check.pkg_cran_remote <- function(x, ...) {
-  pkg_metric_todo("Assessment of R CMD check on remote pkg refs is not yet implemented but will be in the future")
+  as_pkg_metric_todo("Assessment of R CMD check on remote pkg refs is not yet implemented but will be in the future")
 }
 
 #' @export
 assess_r_cmd_check.pkg_bioc_remote <- function(x, ...) {
-  pkg_metric_todo("Assessment of R CMD check on remote pkg refs is not yet implemented but will be in the future")
+  as_pkg_metric_todo("Assessment of R CMD check on remote pkg refs is not yet implemented but will be in the future")
 }
 
 #' Score a package based on R CMD check results run locally
@@ -42,7 +42,7 @@ assess_r_cmd_check.pkg_bioc_remote <- function(x, ...) {
 #'
 #' @export
 metric_score.pkg_metric_r_cmd_check <- function(x, ...) {
-  sum(x*c(0.1,1,0.5))
+  sum(x*c(0.1, 1, 0.5))
 }
 attributes(metric_score.pkg_metric_r_cmd_check)$label <-
   "A weighted sum of errors/warnings/notes from R CMD Check"
