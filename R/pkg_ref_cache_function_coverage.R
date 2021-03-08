e8db94b0f80f065fd31b6ab2c21f5547a6b15b83
Yilong Zhang <yilong.zhang@merck.com>
2021-03-08 10:42:47 -0500

    update function_coverage to expression_coverage

--- R/pkg_ref_cache_function_coverage.R
+++ R/pkg_ref_cache_function_coverage.R
@@ -1,10 +1,10 @@
-#' Retrieve output of covr::package_coverage, tallied by function
+#' Retrieve output of covr::package_coverage, tallied by expression
 #'
 #' @inheritParams pkg_ref_cache
 #' @family package reference cache
 #'
-pkg_ref_cache.function_coverage <- function(x, ...) {
-  UseMethod("pkg_ref_cache.function_coverage")
+pkg_ref_cache.expression_coverage <- function(x, ...) {
+  UseMethod("pkg_ref_cache.expression_coverage")
 }
 
 #' @importFrom covr tally_coverage
