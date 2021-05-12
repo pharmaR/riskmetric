f973b426babef19cad5801adb704cd6ef5acb9d4
Eric Milliman <eric.milliman@biogen.com>
2021-05-12 15:03:25 -0400

    commiting all works in progress

--- R/assess_dependencies.R
+++ R/assess_dependencies.R
@@ -1,9 +1,14 @@
 assess_reverse_dependencies <- function(x, ...){
-  
-  
+  return(nrow(x$revdeps))
+
 }
 
-assess_dependencies <- function(x, ...){
-  
-  
+assess_total_dependencies <- function(x, ...){
+  return(table(x$deps$type[!grepl("^R ", x$deps$package)]))
+}
+
+assess_new_dependencies <- function(x, ...){
+  ip <- memoise_installed_packages()
+  newDeps <- x$deps[!gsub("\\(.+\\)","", x$deps$package) %in% rownames(ip), ]
+  return(table(newDeps$type[!grepl("^R ", newDeps$package)]))
 }
--- R/pkg_ref_cache_dependencies.R
+++ R/pkg_ref_cache_dependencies.R
@@ -1,12 +1,21 @@
-
-pkg_ref_cache.revdeps <- function(x, ...){
-  revdeps <- lapply(rownames(pkgs), function(x) {
-    lapply(c("Depends","Imports","LinkingTo"), function(d) { devtools::revdep(x,dependencies = d)
-    })
-  })
-}
-
+#' Cache package dependencies
+#'
+#' A package's dependencies is an important indicator or the package's complexiety.
+#'
+#' @export
 
 pkg_ref_cache.deps <- function(x, ...){
+  UseMethod("pkg_ref_cache.deps")
+}
 
+pkg_ref_cache.deps.pkg_remote <- function(x, ...){
+  ap <- available.packages(contriburl = x$repo)
+  deps <- ap[rownames(ap)==x$name, c("LinkingTo","Imports","Depends")]
+  deps <- deps[!is.na(deps)]
+  deps <- sapply(strsplit(deps, ","), trimws)
+  deps <- data.frame(package=unlist(deps),
+                     type=rep(names(deps), sapply(deps, length)),
+                     stringsAsFactors = FALSE,
+                     row.names = NULL)
+  return(deps)
 }
