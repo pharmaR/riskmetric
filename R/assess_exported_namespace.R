afa8f13c7eca85e0a8585b5fe68f4c37cb54c170
Eric Milliman <eric.milliman@biogen.com>
2021-05-19 10:58:16 -0400

    exported namespace update

--- R/assess_exported_namespace.R
+++ R/assess_exported_namespace.R
@@ -4,20 +4,50 @@
 #'   "r_cmd_check",
 #'   "Tally of errors, warnings and notes from running R CMD check locally",
 #'   dontrun = TRUE)
-
+#'
+#' @importFrom pkgload parse_ns_file
 #' @export
 assess_exported_namespace <- function(x, ...) {
   UseMethod("assess_exported_namespace")
 }
 
-attributes(assess_r_cmd_check)$column_name <- "exported_namespace"
-attributes(assess_r_cmd_check)$label <- "Objects exported by package"
+attributes(assess_exported_namespace)$column_name <- "exported_namespace"
+attributes(assess_exported_namespace)$label <- "Objects exported by package"
 
 #' @export
 assess_exported_namespace.default <- function(x, ...) {
+  as_pkg_metric_na(
+    pkg_metric(class = "pkg_metric_export_help"),
+    message = "Cannot export namesapce from a ")
+}
+
+assess_exported_namespace.pkg_install <- function(x, ...) {
   pkg_metric_eval(class = "pkg_metric_exported_namespace", {
     # ignore S3-dispatched methods
-    return(length(x$exported_namespaces))
+    return(getNamespaceExports(x))
   })
 }
+
+assess_exported_namespace.pkg_source <- function(x, ...) {
+  pkg_metric_eval(class = "pkg_metric_exported_namespace", {
+    # ignore S3-dispatched methods
+    return(pkgload::parse_ns_file()$exports)
+  })
+}
+
+#' Score a package for the number of exported objects
+#'
+#' Count the number of exported objects (excluding S3Methods) and divide by 100
+#'
+#' @eval roxygen_score_family("exported_namespace")
+#' @return numeric value
+#'
+#' @export
+metric_score.pkg_metric_exported_namespace <- function(x, ...) {
+  length(x)
+}
+
+attributes(metric_score.pkg_metric_exported_namespace)$label <-
+  "The number of exported objects in a package"
+
 
