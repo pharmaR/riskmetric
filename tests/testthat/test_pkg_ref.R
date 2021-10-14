d73a1e77decfbcbe1fdb1f05b47f6823b1becd0b
Eli Miller <elimillera@gmail.com>
2021-10-14 18:50:57 -0500

    Switch tests to use source functions (#223)
    * Switch tests to use source functions
    
    * Update for passing RCMD check
    
    * Add additional tests

--- tests/testthat/test_pkg_ref.R
+++ tests/testthat/test_pkg_ref.R
@@ -4,7 +4,7 @@ test_that("get_pkg_ref_classes appropriately captures full class chain", {
   expect_equal(get_pkg_ref_classes("d", abstract_hierarchy), c("d", "c", "a"))
 
   expect_equal(
-    get_pkg_ref_classes("pkg_cran_remote"), 
+    get_pkg_ref_classes("pkg_cran_remote"),
     c("pkg_cran_remote", "pkg_remote", "pkg_ref")
   )
 })
@@ -21,13 +21,13 @@ test_that("pkg_ref infers source when not explicitly provided", {
   expect_equal(ref1$source, "pkg_install")
 
   # pkg_cran_remote
-  ref2 <- pkg_ref("pkgcranremotegood")
+  ref2 <- pkg_cran("pkgcranremotegood")
   expect_s3_class(ref2, c("pkg_cran_remote", "pkg_remote", "pkg_ref", "environment"), exact = TRUE)
   expect_equal(ref2$source, "pkg_cran_remote")
 
   # pkg_bioc_remote
   # TODO: add bioconductor available packages mock and replace with a stub package
-  ref3 <- pkg_ref("GenomicFeatures")
+  ref3 <- pkg_bioc("GenomicFeatures")
   expect_s3_class(ref3, c("pkg_bioc_remote", "pkg_remote", "pkg_ref", "environment"), exact = TRUE)
   expect_equal(ref3$source, "pkg_bioc_remote")
 })
@@ -56,6 +56,7 @@ test_that("pkg_ref throws nice warnings when you give bad 'source' arguments",{
     p1 <- pkg_ref("UnknownCRANPkg", source = "pkg_cran_remote"),
     "Package: `UnknownCRANPkg` not found on CRAN, source is now 'pkg_missing'"
   )
+
   expect_equal(p1$source, "pkg_missing")
 
   expect_warning(
@@ -69,4 +70,50 @@ test_that("pkg_ref throws nice warnings when you give bad 'source' arguments",{
     "Package source: `./MissingPackage` does not exist, source is now 'pkg_missing'"
   )
   expect_equal(p3$source, "pkg_missing")
+})
+
+test_that("pkg_ref throws errors as expected", {
+
+  expect_error(
+    new_pkg_ref("dplyr",
+                source = "pkg_cran_remote",
+                version = "1.0.0",
+                "someUnnamedArgument"),
+    "pkg_ref ellipses arguments must be named"
+    )
+  expect_error(
+    pkg_ref(structure(list("abc"), class = "badClass")),
+    "Don't know how to convert object class 'badClass' to class 'pkg_ref'"
+  )
+
+})
+
+test_that("pkg_ref will return the same object if passed an object of class pkg_ref", {
+
+  ref1 <- pkg_ref("tools")
+
+  expect_equal(ref1, pkg_ref(ref1))
+
+})
+
+test_that("determine_pkg_source returns the expected values", {
+  ## pkg_source
+  expect_equal(
+    determine_pkg_source(file.path(test_path(), "test_packages", "pkgsourcegood"),
+                         repos = getOption("repos")),
+    "pkg_source"
+    )
+
+  ## pkg_install
+  expect_equal(
+    determine_pkg_source("tools", repos = getOption("repos")),
+    "pkg_install"
+  )
+
+  ## pkg_cran_remote
+  expect_equal(
+    determine_pkg_source("pkgcranremotegood", repos = getOption("repos")),
+    "pkg_cran_remote"
+  )
+
 })
