433446d9bad1b9b65684106d6f2b57526a34a3e4
James J. Kim <james.j.kim@pfizer.com>
2022-02-09 17:14:16 -0500

    fixed the typo & workflow issue with NULL return (as.numeric(x>0))

--- R/assess_has_bug_reports_url.R
+++ R/assess_has_bug_reports_url.R
@@ -18,7 +18,7 @@ attr(assess_has_bug_reports_url,"label") <- "presence of a bug reports url in re
 
 
 #' @export
-assess_has_news.pkg_ref <- function(x, ...) {
+assess_has_has_bug_reports_url.pkg_ref <- function(x, ...) {
   pkg_metric(class = "pkg_metric_has_bug_reports_url", {
     length(x$bug_reports_url)
   })
@@ -35,7 +35,7 @@ assess_has_news.pkg_ref <- function(x, ...) {
 #'
 #' @export
 metric_score.pkg_metric_has_bug_reports_url <- function(x, ...) {
-  as.numeric(x > 0)
+  as.numeric(length(x) > 0)
 }
 
 attributes(metric_score.pkg_metric_has_bug_reports_url)$label <-
