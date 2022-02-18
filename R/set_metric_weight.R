17846d5d2cb599a5360a0716ba7988fbecba053d
Eric Milliman <emilliman4@gmail.com>
2022-02-18 13:43:00 -0500

    Convenience function to set metric weights by name without the need to set all weights.

---
+++ R/set_metric_weight.R
@@ -0,0 +1,17 @@
+#' Function to conviently set the weights for a subset of package metrics
+#'
+#' Sometimes you want to change one or a few metric weights while keeping the rest the same,
+#' instead of having to define all weights this function will let you set the weight of
+#' specific metrics while returning the default weight for the rest of the metrics not named.
+#'
+#' @param data `pkg_assess` object
+#' @param weight named vector of assessment(s) and their weight(s)
+#'
+#' @return A named vector of package metrics and their weights
+#'
+#' @export
+set_metric_weight <- function(data, weight){
+  default_weights <- add_default_weights(data)
+  default_weights[names(weight)] <- weight
+  return(default_weights)
+}
