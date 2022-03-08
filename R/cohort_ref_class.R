18c2f6b4867874d2a029ff0977a24bc485d1114a
Eric Milliman <emilliman4@gmail.com>
2022-03-08 17:16:09 -0500

    beefing up cohort_ref class

--- R/cohort_ref_class.R
+++ R/cohort_ref_class.R
@@ -6,26 +6,29 @@
 #' @export
 #' @family cohort_ref
 #'
-cohort_ref <- function(x, library, ...){
+cohort_ref <- function(x, library, includeDependencies = TRUE, ...){
   if (missing(x)) return(structure(list(pkg_ref_list = list(logical(0L)),
                               library = logical(0L)), class = "cohort_ref"))
   new_cohort_ref(x, library, ...)
 }
 
-new_cohort_ref <- function(x, library, ...){
+new_cohort_ref <- function(x, library, includeDependencies = includeDependencies...){
   dots <- list(...)
+
   if (length(dots) && is.null(names(dots)) || any(names(dots) == ""))
     stop("cohort_ref ellipses arguments must be named")
 
-  if(is.character(x) | all(sapply(x, class) != "pkg_ref")){
+  is_a_path <- all(is_path(library))
+  if((is.character(x) & length(x)>1) | all(sapply(x, function(y) !"pkg_ref" %in% class(y)))){
     x <- lapply(x, pkg_cran, repos = "https://cran.rstudio.com")
-  }
+  } else if (class(x) == "list_of_pkg_ref" || all(sapply(x, function(y) "pkg_ref" %in% class(y))) )
 
   if(!missing(library) &&
      (is.character(library) | all(sapply(library, class) != "pkg_ref"))){
-    library <- pkg_ref(library)
+    library <-
     cohort_data <- list(cohort = x, library=library, dots)
   }
+
   cohort_data <- list(cohort = x, library=logical(0L), dots)
   structure(cohort_data, class = "cohort_ref")
 }
@@ -43,4 +46,8 @@ make_library <- function(x){
 
 }
 
-
+is_path <- function(x){
+  if(is.character(x)){
+    return(dir.exists(x))
+  }
+}
