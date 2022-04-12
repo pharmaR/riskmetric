7ca681532af0630594f1e856c1b34e6ec22fe5e6
Eric Milliman <emilliman4@gmail.com>
2022-04-12 16:11:24 -0400

    cohort_score function, akin to pkg_score

---
+++ R/cohort_score.R
@@ -0,0 +1,21 @@
+#' Score a cohort assessment, collapsing results into a single numeric
+#' cohort_score() calculates the risk involved with using a cohort of packages. Risk ranges
+#' from 0 (low-risk) to 1 (high-risk).
+#'
+#' @export
+cohort_score <- function(x, ..., error_handler = score_error_default) {
+  UseMethod("cohort_score")
+}
+
+#' @export
+cohort_score.list_of_cohort_metric <- function(x, ...,
+                                         error_handler = score_error_default) {
+
+  lapply(x, function(xi) {
+    s <- metric_score(xi, error_handler = error_handler)
+    metric_score_s3_fun <- firstS3method("metric_score", class(xi))
+    attr(s, "label") <- attr(metric_score_s3_fun, "label")
+    class(s) <- c("cohort_score", class(s))
+    s
+  })
+}
