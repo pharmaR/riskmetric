0656aa54c3efa1c0ccb570f338f9afc94eb8e79d
Eric Milliman <emilliman4@gmail.com>
2022-04-13 10:35:57 -0400

    added metric_score for cohort_ref assessment. fixed class typo in assessment return object

--- R/assess_exported_namespace.R
+++ R/assess_exported_namespace.R
@@ -40,7 +40,7 @@ assess_exported_namespace.pkg_source <- function(x, ...) {
 #' @export
 assess_exported_namespace.cohort_ref <- function(x, ...) {
   ns <- unlist(lapply(x$cohort, assess_exported_namespace))
-  return(cohort_metric_eval(class = "cohort_metric_exported_namespaces",
+  return(cohort_metric_eval(class = "cohort_metric_exported_namespace",
                             ns
   ))
 }
@@ -70,3 +70,29 @@ metric_score.pkg_metric_exported_namespace <- function(x, ...) {
 
 attributes(metric_score.pkg_metric_exported_namespace)$label <-
   "The number of exported objects in a package"
+
+
+#' Score a cohort for the number of exported objects
+#'
+#' Score a cohort for the number of exported objects it has; regularized
+#' Convert the number of exported objects \code{length(x)} into a validation
+#' score [0,1] \deqn{ 1 / (1 + exp(-0.5 * (sqrt(length(x)) + sqrt(5)))) }
+#'
+#' The scoring function is the classic logistic curve \deqn{
+#' 1 / (1 + exp(-k(x-x[0])) } with a square root scale for the number of exported objects
+#' \eqn{x = sqrt(length(x))}, sigmoid midpoint is 25 exported objects, ie. \eqn{x[0] =
+#' sqrt(5)}, and logistic growth rate of \eqn{k = 0.25}.
+#'
+#' \deqn{ 1 / (1 + exp(-0.25 * sqrt(length(x))-sqrt(25))) }
+#'
+#' @eval roxygen_score_family("exported_namespace")
+#' @return numeric value between \code{0} (high number of exported objects) and
+#'   \code{1} (low number of exported objects)
+#'
+#' @export
+metric_score.cohort_metric_exported_namespace <- function(x, ...) {
+  1 - 1 / (1 + exp(-0.25 * (sqrt(length(x)) - sqrt(25))))
+}
+
+attributes(metric_score.cohort_metric_exported_namespace)$label <-
+  "The number of exported objects in a cohort"
