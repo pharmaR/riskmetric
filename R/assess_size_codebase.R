7f828851fd17f2090ebc93126abcb44d25d3f510
Wang, Sheng-Wei [JRDUS] <swang69@ITS.JNJ.com>
2022-07-14 13:36:07 -0400

    use mget function to obtain the line of codes

--- R/assess_size_codebase.R
+++ R/assess_size_codebase.R
@@ -16,12 +16,15 @@ attributes(assess_size_codebase)$label <- "number of lines of code base"
 assess_size_codebase.pkg_install <- function(x, ...) {
   pkg_metric_eval(class = "pkg_metric_size_codebase", {
     # create character vector of exported function
-    exports <- paste0(x$name,"::", getNamespaceExports(x$name))
+    exports <- getNamespaceExports(x$name)
 
-    # count number of lines in each function through capture.output function
-    nloc <- sapply(exports, function(x){length(capture.output(eval(str2lang(x))))})
+    # add package to the search path
+    attachNamespace(x$name)
 
-    # sum the number of lines and extract the 2 extra lines (bytecode, environment)
-    sum(nloc) - 2*length(nloc)
+    # count number of lines for exported function through the mget function
+    nloc <- capture.output(mget(exports, envir = as.environment(paste0("package:", x$name))))
+
+    # sum the number of lines and extract the 4 extra lines (function name, bytecode, environment, blank line)
+    length(nloc) - 4*length(exports)
   })
 }
