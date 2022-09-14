51ba08c0cbd3d80a61bb45ee6174460e6e82eb30
Wang, Sheng-Wei [JRDUS] <swang69@ITS.JNJ.com>
2022-09-14 12:48:50 -0400

    add the function for pkg_source class

--- R/assess_size_codebase.R
+++ R/assess_size_codebase.R
@@ -28,3 +28,40 @@ assess_size_codebase.pkg_install <- function(x, ...) {
     length(nloc) - 4*length(exports)
   })
 }
+
+#' @export
+assess_size_codebase.pkg_source <- function(x, ...) {
+  pkg_metric_eval(class = "pkg_metric_size_codebase", {
+    # create character vector of function files
+    files <- list.files(path = file.path(x$path, "R"), full.names = T)
+
+    # define the function for counting code base
+    count_lines <- function(x){
+      # read the lines of code into a character vector
+      code_base <- readLines(x)
+
+      # count all the lines
+      n_tot <- length(code_base)
+
+      # count lines for roxygen headers starting with #
+      n_head <- length(grep("^#+", code_base))
+
+      # count the comment lines with leading spaces
+      n_comment <- length(grep("^\\s+#+", code_base))
+
+      # count the line breaks or only white space lines
+      n_break <- length(grep("^\\s*$", code_base))
+
+      # compute the line of code base
+      n_tot - (n_head + n_comment + n_break)
+    }
+
+    # count number of lines for all functions
+    nloc <- sapply(files, count_lines)
+
+    # sum the number of lines
+    sum(nloc)
+  })
+}
+
+
