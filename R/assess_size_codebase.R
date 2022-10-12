0f44f48a557bca76ba3b9b4e573668248bc5304e
Eric Milliman <emilliman4@gmail.com>
2022-10-12 15:11:10 -0400

    Added default method for assess_size_codebase. added metric_score method for assess_size_codebase. updated documentation.

--- R/assess_size_codebase.R
+++ R/assess_size_codebase.R
@@ -13,6 +13,14 @@ attributes(assess_size_codebase)$column_name <- "size_codebase"
 attributes(assess_size_codebase)$label <- "number of lines of code base"
 
 #' @export
+assess_size_codebase.default <- function(x, ...) {
+  as_pkg_metric_na(
+    pkg_metric(class = "pkg_metric_size_codebase"),
+    message = sprintf("Cannot compute the number of lines of code from a %s", x$source))
+}
+
+
+#' @export
 assess_size_codebase.pkg_install <- function(x, ...) {
   pkg_metric_eval(class = "pkg_metric_size_codebase", {
     # create character vector of exported function
@@ -25,3 +33,18 @@ assess_size_codebase.pkg_install <- function(x, ...) {
     sum(nloc) - 2*length(nloc)
   })
 }
+
+#' Score a package for number of lines of code
+#'
+#' Scores packages based on its codebase size, as determined by number of lines of code.
+#'
+#' @eval roxygen_score_family("size_codebase")
+#'
+#' @return numeric value between \code{0} (low) and \code{1} (large number of lines of code) converting the number of downloads.
+#' @export
+metric_score.pkg_metric_size_codebase <- function(x, ...) {
+  1.5 / (x / 1e2 + 1.5) - 1
+}
+
+attributes(metric_score.pkg_metric_size_codebase)$label <-
+  "A logistic rating of the number of lines of code in a package."
