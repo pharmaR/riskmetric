b8950b2e82eafefc24bd0e46c697222d87c744fb
Andrew Borgman <andrew.borgman@biogen.com>
2023-02-02 20:54:40 -0500

    update pkg_ref_cache_security to just follow simple default

--- R/pkg_ref_cache_security.R
+++ R/pkg_ref_cache_security.R
@@ -9,21 +9,19 @@ pkg_ref_cache.security <- function (x, ...)
   UseMethod("pkg_ref_cache.security")
 }
 
-pkg_ref_cache.security.default <- function (x, ...) {
-  return(NA)
-}
-
-#' Check if oyster
+#' Check OSS Index lists any vulnerabilities for the package
 #'
 #' @inheritParams pkg_ref_cache
 #' @importFrom oysteR audit
 #' @return a \code{pkg_ref} object
-pkg_ref_cache.security.pkg_source <- function(x, ...){
+pkg_ref_cache.security.default <- function(x, ...){
 
-  # TODO
+  # TODO: confirm this just be default as oysteR now works on name + version and
+  #       those are present for all pkg_ref instances?
+
   scan_results <- oysteR::audit(
-    pkg = "name",
-    version = "vers", # will this need to be pkg_ref class specific?
+    pkg = x$name,
+    version = x$version, # will this need to be pkg_ref class specific?
     type = "cran", # will this need to be pkg_ref class specific?
     verbose = FALSE
     )
