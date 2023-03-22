c62cf3459fb857f58f3d94beedf5f905814acbe9
Sam Parmar <107635309+parmsam-pfizer@users.noreply.github.com>
2023-03-22 11:07:52 -0500

    add expectation to check earliest downloads date

--- tests/testthat/test_pkg_ref_cache_downloads.R
+++ tests/testthat/test_pkg_ref_cache_downloads.R
@@ -5,4 +5,8 @@ test_that("pkg_ref_cache_downloads returns expected result for source package", 
     pkg_ref_source_good$downloads,
     c("date", "count", "package")
   )
+  testthat::expect_equal(
+    as.character(pkg_ref_source_good$downloads[[1,1]]),
+    "2012-10-01"
+  )
 })
