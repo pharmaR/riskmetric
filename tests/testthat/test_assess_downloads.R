141765920edc7e3db1a2e7732bf42f7dd2e6fb19
Sam Parmar <107635309+parmsam-pfizer@users.noreply.github.com>
2023-03-21 22:01:44 -0500

    add unit test for future

---
+++ tests/testthat/test_assess_downloads.R
@@ -0,0 +1,9 @@
+test_that("assess_downloads returns expected result for source package", {
+  expect_s3_class(
+    assess_source_good$downloads_1yr,
+    c("pkg_metric_downloads_1yr", "pkg_metric", "integer"))
+
+  expect_type(assess_source_good$downloads_1yr, "integer")
+  expect_lte(assess_source_good$downloads_1yr, 1)
+  expect_gte(assess_source_good$downloads_1yr, 0)
+})
