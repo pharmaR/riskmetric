c3fea3b7b2d3b4f730df0ec73e972a9c43710b11
Sam Parmar <107635309+parmsam-pfizer@users.noreply.github.com>
2023-03-21 23:28:32 -0500

    test typo fix

--- tests/testthat/test_pkg_ref_cache_downloads.R
+++ tests/testthat/test_pkg_ref_cache_downloads.R
@@ -1,8 +1,8 @@
 test_that("pkg_ref_cache_downloads returns expected result for source package", {
-  expect_type(pkg_ref_source_good$download, "list")
-  testthat::expect_length(pkg_ref_source_good$download, 3)
+  expect_type(pkg_ref_source_good$downloads, "list")
+  testthat::expect_length(pkg_ref_source_good$downloads, 3)
   testthat::expect_named(
-    pkg_ref_source_good$download,
+    pkg_ref_source_good$downloads,
     c("date", "count", "package")
   )
 })
