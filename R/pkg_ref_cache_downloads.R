55af925fb77e66b457bfb0b7650235767527810a
Sam Parmar <107635309+parmsam-pfizer@users.noreply.github.com>
2023-03-30 14:05:39 -0400

    add default and pkg_cran_remote classes for downloads metric

--- R/pkg_ref_cache_downloads.R
+++ R/pkg_ref_cache_downloads.R
@@ -7,7 +7,20 @@
 #' @return a \code{pkg_ref} object
 #' @keywords internal
 pkg_ref_cache.downloads <- function(x, ...) {
-  # Downloads since October of 2012, the month RStudio CRAN mirror started publishing logs
+  UseMethod("pkg_ref_cache.downloads")
+}
+
+#' @importFrom cranlogs cran_downloads
+#' @keywords internal
+pkg_ref_cache.downloads.default <- function(x, ...) {
+  # Downloads since Oct2012 - when RStudio CRAN mirror started publishing logs
   from_date <- as.Date("2012-10-01", format = "%Y-%m-%d")
+  cran_downloads(x$name, from = from_date, to = Sys.Date())
+}
+
+#' @importFrom cranlogs cran_downloads
+#' @keywords internal
+pkg_ref_cache.downloads.pkg_cran_remote <- function(x, name, ...) {
+  from_date <- x$archive_release_dates[1, 'date'][[1]]
   cran_downloads(x$name, from = from_date, to = Sys.Date())
 }
