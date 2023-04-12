ab6b40fa8d385dacc87dcae3d256255bcbd3f810
Eric Milliman <emilliman4@gmail.com>
2023-04-12 17:14:22 -0400

    tests for assess and scoring of export_help

--- tests/testthat/test_assess_export_help.R
+++ tests/testthat/test_assess_export_help.R
@@ -1,5 +1,11 @@
 # Commenting these out until this is implemented for sourced packages
-# test_that("assess_export_help returns expected result for source packages", {
-#   expect_true(assess_source_1$export_help[[1]])
-#   expect_false(assess_source_2$export_help[[1]])
-# })
+test_that("assess_export_help returns expected result for source packages", {
+  expect_true(assess_source_good$export_help[[1]])
+  expect_true(assess_install_good$export_help[[1]])
+  expect_true(assess_source_bad$export_help[[1]])
+  expect_true(assess_stdlibs_install$export_help[[1]][[1]])
+
+  expect_true(is.numeric(score_install_good$export_help))
+  expect_true(is.numeric(score_source_bad$export_help))
+  expect_true(is.numeric(score_source_good$export_help))
+})
