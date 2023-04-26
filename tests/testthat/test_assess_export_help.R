1e062ae0d23afde3325f7a37646001caa04ac145
elimillera <elimillera@gmail.com>
2023-04-26 14:48:02 +0000

    Update export_help tests to expect_equal

--- tests/testthat/test_assess_export_help.R
+++ tests/testthat/test_assess_export_help.R
@@ -1,11 +1,16 @@
 # Commenting these out until this is implemented for sourced packages
 test_that("assess_export_help returns expected result for source packages", {
-  expect_true(assess_source_good$export_help[[1]])
-  expect_true(assess_install_good$export_help[[1]])
-  expect_true(assess_source_bad$export_help[[1]])
+
+  expect_equal(unname(assess_source_good$export_help), c(TRUE))
+  expect_equal(unname(assess_install_good$export_help), c(TRUE))
+
+  expect_equal(unname(assess_source_bad$export_help), c(FALSE, TRUE))
+  expect_equal(unname(assess_install_bad$export_help), c(FALSE, TRUE))
+
   expect_true(assess_stdlibs_install$export_help[[1]][[1]])
 
-  expect_true(is.numeric(score_install_good$export_help))
-  expect_true(is.numeric(score_source_bad$export_help))
-  expect_true(is.numeric(score_source_good$export_help))
+  expect_equal(score_install_good$export_help[[1]], 1)
+  expect_equal(score_install_bad$export_help[[1]], 0.5)
+  expect_equal(score_source_bad$export_help[[1]], 0.5)
+  expoct_equal(score_source_good$export_help[[1]], 1)
 })
