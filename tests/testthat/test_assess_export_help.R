858cb4feee018b6c19811f3cf38bfd9ba6375d32
elimillera <elimillera@gmail.com>
2023-04-26 15:16:25 +0000

    ignore attributes in tests

--- tests/testthat/test_assess_export_help.R
+++ tests/testthat/test_assess_export_help.R
@@ -1,14 +1,14 @@
 # Commenting these out until this is implemented for sourced packages
 test_that("assess_export_help returns expected result for source packages", {
 
-  expect_equal(unclass(unname(assess_source_good$export_help)), c(TRUE))
-  expect_equal(unclass(unname(assess_install_good$export_help)), c(TRUE))
+  expect_equal(unclass(unname(assess_source_good$export_help)), c(TRUE), ignore_attr = TRUE)
+  expect_equal(unclass(unname(assess_install_good$export_help)), c(TRUE), ignore_attr = TRUE)
 
-  expect_equal(unclass(unname(assess_source_bad$export_help)), c(FALSE, TRUE))
+  expect_equal(unclass(unname(assess_source_bad$export_help)), c(FALSE, TRUE), ignore_attr = TRUE)
 
   expect_true(assess_stdlibs_install$export_help[[1]][[1]])
 
-  expect_equal(score_install_good$export_help[[1]], 1)
-  expect_equal(score_source_bad$export_help[[1]], 0.5)
-  expoct_equal(score_source_good$export_help[[1]], 1)
+  expect_equal(score_install_good$export_help[[1]], 1), ignore_attr = TRUE
+  expect_equal(score_source_bad$export_help[[1]], 0.5, ignore_attr = TRUE)
+  expect_equal(score_source_good$export_help[[1]], 1, ignore_attr = TRUE)
 })
