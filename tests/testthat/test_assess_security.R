3f5f9b83692438980cd576c5a2094f9ef26512ed
aborgman <andrew.borgman@biogen.com>
2023-06-12 15:49:44 -0400

    notes

--- tests/testthat/test_assess_security.R
+++ tests/testthat/test_assess_security.R
@@ -16,12 +16,19 @@ test_that("assess_security returns expected result for test packages", {
 
 
   # txt <- capture.output({tmp <- oysteR::audit(c("widgetframe", "ggplot2"), c("0.3.1", "1.0.0"), "cran")}, type = "message")
+  # txt2 <- capture.output({tmp <- oysteR::audit("fakepackage", "cran")}, type = "message")
+
+
+  # cant "know" what is missing
+  # https://github.com/sonatype-nexus-community/oysteR/issues/62
+
 
 
   # should we also cache what comes out of vulnerabilities
 
 
-  expect_numeric(assess_source_good$security)
+  # expect_numeric(assess_source_good$security)
+  browser()
   expect_equal(assess_source_good$security, 0)
 
   # pkg_assess(assess_source_good, assessments = all_assessments(include_suggests = T))
