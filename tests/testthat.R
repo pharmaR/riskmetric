1d2e43da48cd4dc09e4c7bc039cdf19d84d32071
Lluís Revilla <lluis.revilla_sancho@roche.com>
2025-03-13 12:41:53 +0100

    Check that suggested packages used on tests are available

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,8 @@
-library(testthat)
-library(riskmetric)
+if (requireNamespace("testthat", quietly = TRUE) && requireNamespace("webmockr", quietly = TRUE) && requireNamespace("jsonlite", quietly = TRUE) && requireNamespace("magrittr", quietly = TRUE)  && requireNamespace("withr", quietly = TRUE)) {
+  library(testthat)
+  library(riskmetric)
 
-options(repos = "fake-cran.fake-r-project.org")
+  options(repos = "fake-cran.fake-r-project.org")
 
-test_check("riskmetric")
+  test_check("riskmetric")
+}
